5f3d1a52a0e4f301ce6050af0e5cae01a7b86ef2
Lip Cowan <pcowan90@gmail.com>
2020-11-11 16:29:38 -0700

    cleaning up

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders landing page', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/LipCo/i);
-  expect(linkElement).toBeInTheDocument();
-});
